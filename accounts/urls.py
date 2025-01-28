80139caed7b01e72388121da786442924fa1c68e
eayerishz <esmarimla@student.hau.edu.ph>
2025-01-28 19:46:11 +0800

    requirements.txt

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('account/', views.get_users, name='get_account'),
-    path('account/<uuid:user_uuid>/', views.get_user, name='get_account'),
+    path('users/', views.get_users, name='get_users'),
+    path('user/<uuid:user_uuid>/', views.get_user, name='get_user'),
 ]
--- backend/settings.py
+++ backend/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -114,6 +115,8 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
+
+CORS_ALLOW_ALL_ORIGINS =True
 
 
 # Static files (CSS, JavaScript, Images)
